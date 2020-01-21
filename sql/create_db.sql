7709177c2a4de43858be1c30c0b1981d359ff260
Artyom <onedeadwave@gmail.com>
2020-01-21 15:45:09 +1000

    Changed db structure

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -2,10 +2,11 @@ CREATE TABLE "notes" (
 	"id" serial NOT NULL,
 	"author_id" int NOT NULL,
 	"category_id" int NOT NULL,
+	"subject_id" int NOT NULL,
 	"teacher_id" int NOT NULL,
+	"posted_at" TIMESTAMP NOT NULL,
 	"title" TEXT NOT NULL,
-	"description" TEXT,
-	"posted_at" TIMESTAMP WITH TIME ZONE NOT NULL,
+	"descirption" TEXT NOT NULL,
 	"link" TEXT NOT NULL UNIQUE,
 	CONSTRAINT "notes_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -20,7 +21,6 @@ CREATE TABLE "users" (
 	"password" TEXT,
 	"email" varchar(255) NOT NULL,
 	"about" TEXT NOT NULL,
-	"image" TEXT,
 	CONSTRAINT "users_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -30,7 +30,6 @@ CREATE TABLE "users" (
 
 CREATE TABLE "categories" (
 	"id" serial NOT NULL,
-	"subject" int,
 	"name" TEXT NOT NULL,
 	"description" TEXT,
 	CONSTRAINT "categories_pk" PRIMARY KEY ("id")
@@ -42,6 +41,7 @@ CREATE TABLE "categories" (
 
 CREATE TABLE "subjects" (
 	"id" serial NOT NULL,
+	"category_id" int NOT NULL,
 	"name" TEXT NOT NULL,
 	CONSTRAINT "subjects_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -52,7 +52,7 @@ CREATE TABLE "subjects" (
 
 CREATE TABLE "teachers" (
 	"id" serial NOT NULL,
-	"subject_id" int NOT NULL,
+	"category_id" int NOT NULL,
 	"name" TEXT NOT NULL UNIQUE,
 	CONSTRAINT "teachers_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -63,10 +63,11 @@ CREATE TABLE "teachers" (
 
 ALTER TABLE "notes" ADD CONSTRAINT "notes_fk0" FOREIGN KEY ("author_id") REFERENCES "users"("id");
 ALTER TABLE "notes" ADD CONSTRAINT "notes_fk1" FOREIGN KEY ("category_id") REFERENCES "categories"("id");
-ALTER TABLE "notes" ADD CONSTRAINT "notes_fk2" FOREIGN KEY ("teacher_id") REFERENCES "teachers"("id");
+ALTER TABLE "notes" ADD CONSTRAINT "notes_fk2" FOREIGN KEY ("subject_id") REFERENCES "subjects"("id");
+ALTER TABLE "notes" ADD CONSTRAINT "notes_fk3" FOREIGN KEY ("teacher_id") REFERENCES "teachers"("id");
 
 
-ALTER TABLE "categories" ADD CONSTRAINT "categories_fk0" FOREIGN KEY ("subject") REFERENCES "subjects"("id");
 
+ALTER TABLE "subjects" ADD CONSTRAINT "subjects_fk0" FOREIGN KEY ("category_id") REFERENCES "categories"("id");
 
-ALTER TABLE "teachers" ADD CONSTRAINT "teachers_fk0" FOREIGN KEY ("subject_id") REFERENCES "subjects"("id");
+ALTER TABLE "teachers" ADD CONSTRAINT "teachers_fk0" FOREIGN KEY ("category_id") REFERENCES "categories"("id");
