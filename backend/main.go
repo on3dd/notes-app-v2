6f2ad529bc6f533a02a2a43662de565fe5cf1ed9
Artyom <onedeadwave@gmail.com>
2020-01-25 16:59:38 +1000

    Added graceful shutdown

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -19,6 +23,17 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
+
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
+
+		<-sigint
+
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+	}()
 
 	log.Printf("Server started at port %v\n", server.Addr)
 	log.Fatal(server.ListenAndServe())
