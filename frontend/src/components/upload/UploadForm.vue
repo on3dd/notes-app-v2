a50d884b294382660ffe9791efd0184664697ef0
Artyom <onedeadwave@gmail.com>
2020-02-05 18:30:18 +1000

    Updated upload form validation

--- frontend/src/components/upload/UploadForm.vue
+++ frontend/src/components/upload/UploadForm.vue
@@ -1,43 +1,65 @@
 <template>
   <v-container>
-    <v-form id="form" lazy-validation onsubmit="return false;" v-model="valid">
-      <span class="display-2 display-sm-3 text-center d-block mb-3">Загрузка работы</span>
-      <v-text-field
-          :rules="titleRules" label="Название" name="title" required v-model="title" />
-      <v-textarea
-          :rules="descriptionRules" hint="Введите описание работы" label="Описание" name="descirption"
-          v-model="description" />
-      <v-file-input
-          :rules="fileRules" accept="application/pdf" label="Файлы" name="file" required v-model="file" />
-      <v-select
-          :items="allCategories" :rules="categoryRules" @change="fetchData(category)" item-text="name" label="Категория"
-          name="category" required v-model="category"/>
-      <v-select
-          :items="allSubjects" :rules="subjectRules" @change="updateSubject" item-text="name" label="Предмет"
-          name="subject" required v-model="subject"/>
-      <v-select
-          :items="allTeachers" :rules="teacherRules" @change="updateTeacher" item-text="name" label="Преподаватель"
-          name="teacher" required v-model="teacher" />
-      <v-checkbox
-          :rules="[v => !!v || 'Вы должны принять условия пользования!']"
-          label="Я согласен с условиями пользования сервисом"
-          required
-          v-model="checkbox"/>
-      <div class="d-flex justify-center">
-        <v-btn
-            :disabled="!valid" @click="submit" class="mx-4" color="success" large type="submit">
-          Отправить
-        </v-btn>
-      </div>
-    </v-form>
+    <ValidationObserver ref="obs" v-slot="{ invalid, validated, passes, validate }">
+      <v-form id="form" lazy-validation onsubmit="return false;" v-model="valid">
+        <span class="display-2 display-sm-3 text-center d-block mb-3">Загрузка работы</span>
+
+        <VTextFieldWithValidation :counter="20" label="Название" rules="required|max:20" v-model="title"/>
+
+        <v-textarea hint="Введите описание работы" label="Описание" name="descirption" v-model="description"/>
+
+        <ValidationProvider rules="required" v-slot="{ errors }">
+          <v-file-input accept="application/pdf" label="Файлы" name="file" v-model="file"/>
+          <div class="v-text-field__details">
+            <div class="v-messages theme--light error--text" role="alert">
+              <div class="v-messages__wrapper">
+                <div class="v-messages__message">{{ errors[0] }}</div>
+              </div>
+            </div>
+          </div>
+        </ValidationProvider>
+
+        <VSelectWithValidation rules="required" :items="allCategories" @change="fetchData(category)"
+          v-model="category" name="category" label="Категория" item-text="name"/>
+
+        <VSelectWithValidation rules="required" :items="allSubjects" @change="updateSubject"
+          v-model="subject" name="subject" label="Предмет" item-text="name"/>
+
+        <VSelectWithValidation rules="required" :items="allTeachers" @change="updateTeacher"
+          v-model="teacher" name="teacher" label="Преподаватель" item-text="name"/>
+
+        <v-checkbox
+            :rules="[v => !!v || 'Вы должны принять условия пользования!']"
+            label="Я согласен с условиями пользования сервисом"
+            required
+            v-model="checkbox"/>
+
+        <div class="d-flex justify-center">
+          <v-btn
+              :disabled="invalid" @click="submit" class="mx-4" color="success" large type="submit">
+            Отправить
+          </v-btn>
+        </div>
+      </v-form>
+    </ValidationObserver>
   </v-container>
 </template>
 
 <script>
+  import {ValidationObserver, ValidationProvider} from "vee-validate";
+  import VTextFieldWithValidation from '../inputs/VTextFieldWithValidation';
+  import VSelectWithValidation from '../inputs/VSelectWithValidation';
+
   import {mapActions, mapGetters} from 'vuex'
 
   export default {
     name: "UploadForm",
+    components: {
+      ValidationObserver,
+      ValidationProvider,
+      VTextFieldWithValidation,
+      VSelectWithValidation
+    },
     data: () => ({
       title: '',
       description: '',
@@ -45,29 +67,9 @@
       subject: '',
       teacher: '',
 
-      valid: false,
-      titleRules: [
-        v => !!v || 'Поле должно быть заполнено',
-        v => (v && v.length >= 10) || 'Название должно содержать как минимум 10 символов',
-      ],
-
       file: null,
       fileRules: [
         v => !!v || "Поле должно быть заполнено"
-      ],
-
-      descriptionRules: [],
-
-      categoryRules: [
-        v => !!v || 'Поле должно быть заполнено',
-      ],
-
-      subjectRules: [
-        v => !!v || 'Поле должно быть заполнено',
-      ],
-
-      teacherRules: [
-        v => !!v || 'Поле должно быть заполнено',
       ],
 
       checkbox: false,
@@ -117,15 +119,6 @@
     },
     computed: {
       ...mapGetters(["allCategories", "allSubjects", "allTeachers", "getCategoryIndex", "getSubjectIndex", "getTeacherIndex"]),
-      // getCategoryIndex() {
-      //   return this.$store.getters.getCategoryIndex(this.category)
-      // },
-      // getSubjectIndex() {
-      //   return this.$store.getters.getSubjectIndex(this.subject)
-      // },
-      // getTeacherIndex() {
-      //   return this.$store.getters.getTeacherIndex(this.teacher)
-      // },
     },
     async mounted() {
       await this.fetchCategories()
